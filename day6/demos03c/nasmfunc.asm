5d8ea4eb3c5150c2f2edc4ce2539fa7550b7a742
Hayato Nishimura <nhayato@nhayato.com>
2020-08-02 15:11:34 +0900

    format file

--- day6/demos03c/nasmfunc.asm
+++ day6/demos03c/nasmfunc.asm
@@ -70,14 +70,15 @@ io_store_eflags:
     PUSH    EAX
     POPFD
     RET
-load_gdtr:		; void load_gdtr(int limit, int addr);
-		MOV		AX,[ESP+4]		; limit
-		MOV		[ESP+6],AX
-		LGDT	[ESP+6]
-		RET
 
-load_idtr:		; void load_idtr(int limit, int addr);
-		MOV		AX,[ESP+4]		; limit
-		MOV		[ESP+6],AX
-		LIDT	[ESP+6]
-		RET
+load_gdtr:  ; void load_gdtr(int limit, int addr);
+    MOV     AX,[ESP+4]      ; limit
+    MOV     [ESP+6],AX
+    LGDT    [ESP+6]
+    RET
+
+load_idtr:  ; void load_idtr(int limit, int addr);
+    MOV     AX,[ESP+4]      ; limit
+    MOV     [ESP+6],AX
+    LIDT    [ESP+6]
+    RET
