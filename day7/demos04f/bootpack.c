b5edcca928cc728996547ff859283638ed214fa9
Hayato Nishimura <nhayato@nhayato.com>
2020-08-03 17:03:13 +0900

    Add interrupt mouse

---
+++ day7/demos04f/bootpack.c
@@ -0,0 +1,85 @@
+#include "bootpack.h"
+
+extern struct FIFO8  keyfifo;
+
+#define PORT_KEYDAT             0x0060
+#define PORT_KEYSTA             0x0064
+#define PORT_KEYCMD             0x0064
+#define KEYSTA_SEND_NOTREADY    0x02
+#define KEYCMD_WRITE_MODE       0x60
+#define KBC_MODE                0x47
+
+// キーボードコントローラがデータ送信可能になるのを待つ
+void wait_KBC_sendready(void)
+{
+    for (;;) {
+        if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+            break;
+        }
+    }
+    return;
+}
+
+// キーボードコントローラの初期化
+void init_keyboard(void)
+{
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, KBC_MODE);
+    return;
+}
+
+#define KEYCMD_SENDTO_MOUSE     0xd4
+#define MOUSECMD_ENABLE         0xf4
+
+// マウス有効
+void enable_mouse(void)
+{
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    return; // うまく行くとACK(0xfa)が送信されてくる
+}
+
+void HariMain(void)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+	char s[40], mcursor[256], keybuf[32];
+    int mx, my, i;
+
+	init_gdtidt();
+	init_pic();
+    io_sti();
+
+    fifo8_init(&keyfifo, 32, keybuf);
+    io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+    io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+
+    init_keyboard();
+
+    init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+    mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
+    my = (binfo->scrny - 28 - 16) /2;
+    init_mouse_cursor8(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16 ,mx ,my ,mcursor, 16);
+    mysprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+    enable_mouse();
+
+    for (;;) {
+        io_cli();
+        if (fifo8_status(&keyfifo) == 0){
+            io_stihlt();
+        } else {
+            i = fifo8_get(&keyfifo);
+            io_sti();
+            mysprintf(s, "%X", i);
+            boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+            putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+        }
+    }
+}
