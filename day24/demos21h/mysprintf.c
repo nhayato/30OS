1f2526fdc57dbb49016ef0739dbcd6d2139565e5
Hayato Nishimura <nhayato@nhayato.com>
2021-03-17 16:57:58 +0900

    [WIP] fix bug

---
+++ day24/demos21h/mysprintf.c
@@ -0,0 +1,80 @@
+#include <stdarg.h>
+
+// 10進数からASCIIコードに変換
+int dec2asc(char *str, int dec) {
+  int len = 0, len_buf;
+  int buf[10];
+  for (;;) {  // 10で割れた回数（桁数）をlenに，各桁をbufに格納
+    buf[len++] = dec % 10;
+    if (dec < 10) break;
+    dec /= 10;
+  }
+  len_buf = len;
+  while (len) {
+    *(str++) = buf[--len] + 0x30;
+  }
+  return len_buf;
+}
+
+// 16進数からASCIIコードに変換
+int hex2asc(char *str, int dec) {
+  int len = 0, len_buf;
+  int buf[10];
+  for (;;) {
+    buf[len++] = dec % 16;
+    if (dec < 16) break;
+    dec /= 16;
+  }
+  len_buf = len;
+  while (len) {
+    len--;
+    *(str++) = (buf[len] < 10) ? (buf[len] + 0x30) : (buf[len] - 9 + 0x60);
+  }
+  return len_buf;
+}
+
+// 16進数からキャピタルASCIIコードに変換
+int hex2asccap(char *str, int dec) {
+  int len = 0, len_buf;
+  int buf[10];
+  for (;;) {
+    buf[len++] = dec % 16;
+    if (dec < 16) break;
+    dec /= 16;
+  }
+  len_buf = len;
+  while (len) {
+    len--;
+    *(str++) = (buf[len] < 10) ? (buf[len] + 0x30) : (buf[len] - 9 + 0x40);
+  }
+  return len_buf;
+}
+
+void mysprintf(char *str, char *fmt, ...) {
+  va_list list;
+  int i, len;
+  va_start(list, fmt);
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 'd':
+          len = dec2asc(str, va_arg(list, int));
+          break;
+        case 'x':
+          len = hex2asc(str, va_arg(list, int));
+          break;
+        case 'X':
+          len = hex2asccap(str, va_arg(list, int));
+          break;
+      }
+      str += len;
+      fmt++;
+    } else {
+      *(str++) = *(fmt++);
+    }
+  }
+  *str = 0x00;  // 最後にNULLを追加
+  va_end(list);
+}
