fc313f595f5fb0d46eef56a608bda4a308ec16a4
Hayato Nishimura <nhayato@nhayato.com>
2021-04-05 19:31:50 +0900

    make sosu, sosu2, sosu3 and use malloc

---
+++ day28/demos25a/api017.asm
@@ -0,0 +1,12 @@
+GLOBAL api_inittimer
+
+SECTION .text
+
+api_inittimer:		; void api_inittimer(int timer, int data);
+		PUSH	EBX
+		MOV		EDX,17
+		MOV		EBX,[ESP+ 8]		; timer
+		MOV		EAX,[ESP+12]		; data
+		INT		0x40
+		POP		EBX
+		RET
---
+++ day28/demos25a/winhelo.c
@@ -0,0 +1,18 @@
+#include "apilib.h"
+
+char buf[150 * 50];
+
+void HariMain()
+{
+  int win;
+  win = api_openwin(buf, 150, 50, -1, "hello");
+  for (;;)
+  {
+    if (api_getkey(1) == 0x0a)
+    {
+      break; // Enterならbreak
+    }
+  }
+
+  api_end();
+}
