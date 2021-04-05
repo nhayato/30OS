338adffe2697f395c79b0d0e52b03e432a209a90
Hayato Nishimura <nhayato@nhayato.com>
2021-04-05 20:09:21 +0900

    make alloca

---
+++ day28/demos25b/stars.c
@@ -0,0 +1,28 @@
+#include "apilib.h"
+
+unsigned long myrand(void);
+
+void HariMain(void)
+{
+  char *buf;
+  int win, i, x, y;
+  api_initmalloc();
+  buf = api_malloc(150 * 100);
+  win = api_openwin(buf, 150, 100, -1, "stars");
+  api_boxfilwin(win, 6, 26, 143, 93, 0); // 0: 黒色
+  for (i = 0; i < 50; i++)
+  {
+    x = (myrand() % 137) + 6;
+    y = (myrand() % 67) + 26;
+    api_point(win, x, y, 3); // 3: 黄色
+  }
+  for (;;)
+  {
+    if (api_getkey(1) == 0x0a)
+    {
+      break; // Enterならbreak
+    }
+  }
+
+  api_end();
+}
